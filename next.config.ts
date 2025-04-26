2553587c8b79747d39197ad92008737fc7229412
gakutakimoto <gakutaro@gmail.com>
2025-04-26 12:39:24 +0900

    TypeScriptのビルドエラーを無視する設定を追加

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript: {
+    // ⚠️ 警告は表示されますが、ビルドは停止しなくなります
+    ignoreBuildErrors: true,
+  }
 };
 
 export default nextConfig;
